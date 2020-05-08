56dc62687cee6cdb4773846c8f98e709f5a74d57
Subin-Panta <subinpanta9@gmail.com>
2020-05-08 21:02:42 +0545

    started working on front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Route, Switch} from "react-router-dom"
+import Navbar from "./components/Navbar"
+import EditExercise from "./components/EditExercise"
+import ExerciseList from "./components/ExerciseList"
+import CreateExercise from "./components/CreateExercise"
+import CreateUser from "./components/CreateUser"
+import Error from "./components/Error"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ExerciseList}/>
+        <Route exact path="/edit/:id" component={EditExercise}/>
+        <Route exact path="/create" component={CreateExercise}/>
+        <Route exact path="/user" component={CreateUser}/>
+        <Route component={Error} />
+      </Switch>
+      
     </div>
   );
 }
